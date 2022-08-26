a9b2e0e653ee81f706bcd799759099ce9cb185e3
Carlo Castro <89119470+caloy31@users.noreply.github.com>
2022-08-27 01:54:05 +0800

    Add files via upload

---
+++ Castro_learn_kotlin_part1/Castro_learn_kotlin_part1/Data Types &  Variable_Kotlin/Review.kt
@@ -0,0 +1,12 @@
+fun main() {
+  // Write your code below
+  var nameSong: String = "Lies of Goods"
+  var minute: Int = 3
+  var bool: Boolean = true
+
+  println(nameSong)
+  println(minute)
+  println(bool) 
+
+
+}
